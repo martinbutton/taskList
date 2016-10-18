23d47447e64c1105b751947d1f6094a7814ff6d0
Martin Button <marty@outerorbit.org>
2016-10-18 12:57:23 +0100

    Task Create and List display
    Added code to allow for a new task to be created and saved in the DB
    through the UI.  This commit also now properly renders the task list in
    the UI using HTML.  A session variable is also used to cache the task
    list read from the DB.

--- taskView.php
+++ taskView.php
@@ -4,6 +4,55 @@
  * - M.Button.
  */
 
+/* Display HTML code output of rendered task records */
+function displayTasks($sqlrecords) {
+	if ($sqlrecords==null) { return; }
+
+	$taskViewId=0;
+
+	foreach ($sqlrecords as $sqlvalues) {
+		// Set startDate and endDate values
+		$startDate=convertDate($sqlvalues['startDate']);
+		$endDate=convertDate($sqlvalues['endDate']);
+
+		echo "<div id='task" . $taskViewId . "' class='taskPanel' onclick='expandTask(\"task" . $taskViewId . "\");'>\n";
+		echo "<div class='startDateCol'>";
+		echo "Start: " . $startDate;
+		echo "</div>\n";
+		echo "<div class='taskTitleLabelCol'>";
+		echo "Title:";
+		echo "</div>\n";
+		echo "<div class='endDateCol'>";
+		echo "End: " . $endDate;
+		echo "</div>\n";
+		echo "<div id='task" . $taskViewId . "Title' class='taskTitleRow'>";
+		echo $sqlvalues['title'];
+		echo "</div>\n</div>\n";
+
+		$taskViewId++;
+/*
+		echo "Start: " . $startDate . ", End: " . $endDate . "<br>";
+
+		echo "title: " . $sqlvalues['title'];
+		echo ", comments: " . $sqlvalues['comments'];
+		echo ", email: " . $sqlvalues['email'] . "<br>\n";
+*/
+	}
+}
+
+/* Converts date from Unix Timestamp to dd/mmm/yyyy format */
+function convertDate($unixDate) {
+		$dateArray=getdate($unixDate);
+
+		// Get day and see if leading 0 needs to be added
+		$day=$dateArray['mday'];
+		if ($day<10) { $day="0" . $day; }
+
+		$displayDate=$day . "/" . substr($dateArray['month'],0,3) . "/" . $dateArray['year'];
+		return $displayDate;
+}
+
+/* Clean user input to ensure it is safe to use */
 function clean_input($value) {
 	$value=trim($value);
 	$value=stripslashes($value);
@@ -11,6 +60,7 @@ function clean_input($value) {
 	return $value;
 }
 
+/* Validate the users input */
 function validateTask() {
 	// Check Title has been entered.
 	$title=clean_input($_POST['title']);
@@ -19,7 +69,7 @@ function validateTask() {
 		echo "<center>Error: Title Empty.";
 		echo "<p>Back end change not accepted!</p>";
 		echo "<a href='taskList.php' style='color: white;'>Okay</a><br><br></center>"; 
-		return false;
+		return null;
 	}
 
 	// Check the Start date isn't after the End date
@@ -35,23 +85,27 @@ function validateTask() {
 		echo "<center>Error: Start Date Malformed.";
 		echo "<p>Back end change not accepted!</p>";
 		echo "<a href='taskList.php' style='color: white;'>Okay</a><br><br></center>"; 
-		return false;		
+		return null;		
 	}
 
 	if (strlen($dayToo . $monthToo . $yearToo)<1) {
 		echo "<center>Error: End Date Malformed.";
 		echo "<p>Back end change not accepted!</p>";
 		echo "<a href='taskList.php' style='color: white;'>Okay</a><br><br></center>"; 
-		return false;		
+		return null;		
 	}
-
-	return true;
-
-/*	-- Check Code --
 
 	// Convert to Unix timestamp
 	$dateFrom=strtotime($dayFrom . " " . $monthFrom . " " . $yearFrom);
 	$dateToo=strtotime($dayToo . " " . $monthToo . " " . $yearToo);
+
+	// Clean comments input and add to associative array
+	$comments=clean_input($_POST['comments']);
+	$sanitisedPost=array("startDate"=>$dateFrom,"endDate"=>$dateToo,"title"=>$title,"comments"=>$comments);
+
+	return $sanitisedPost;
+
+/*	-- Check Code --
 
 	echo "dateFrom :" . $dateFrom . ", dateToo: " . $dateToo . "<br>";
 
