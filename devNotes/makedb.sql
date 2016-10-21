0d5814d25f410d45e4a110c7559b935dca8dd1e5
Martin Button <marty@outerorbit.org>
2016-10-21 14:40:16 +0100

    Added Licensing and README
    Added a Apache License (LICENSE.txt) and README.md to also explain
    program deployment.  Also performed a tidy in the devNotes folder.

--- devNotes/makedb.sql
+++ devNotes/makedb.sql
@@ -1,3 +1,4 @@
+/* Create Database and required tasks table */
 create database tasklist;
 use tasklist;
 create table tasks(id int not null auto_increment primary key,
@@ -7,7 +8,7 @@ create table tasks(id int not null auto_increment primary key,
 	comments text,
 	email varchar(255));
 
-/* Uncomment below if user account also need creating */
+/* Uncomment below if user account also needs creating */
 
 /*
 create user 'taskuser'@'localhost' identified by 'password';
@@ -16,13 +17,3 @@ grant select,insert,update,delete
 	to 'taskuser'@'localhost';
 alter user 'taskuser'@'astro.outerorbit.org' password expire never;
 */
-
-/* Create test data */
-insert into tasks (startDate, endDate, title, comments, email) values (
-	1000,
-	2000,
-	"Example Task",
-	"This is an example task.",
-	"marty@outerorbit.org");
-
-delete from tasks where startDate=1000;
